e34e204d47350cff65706011ada6f0acb91dab4c
Chris Oliver <excid3@gmail.com>
2012-11-13 18:35:09 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Feedist::Application.config.secret_token = '932d624911bfc2f0cd094fd6ae444c22be0a6f1d8d64a2b942a3be5a6067c4093aed8288aa11f88feb1157ff14f726714b3b78342de81ea84b0be5cbb758b1ea'
