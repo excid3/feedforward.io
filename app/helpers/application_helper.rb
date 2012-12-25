e9b36eb4285fc91736860ce368c6b3bc74942c44
Chris Oliver <excid3@gmail.com>
2012-12-25 10:16:26 -0600

    Update all the things

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 module ApplicationHelper
+  def avatar_image(email, options=nil)
+    options ||= {gravatar: {size: 48, default: "mm"}, class: "img-rounded"}
+    gravatar_image_tag(email, options)
+  end
+
   def nav_link(active_on, &block)
     active = controller_name == active_on ? "active" : ""
     content_tag :li, class: active, &block
