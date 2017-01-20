ada7e6b0deb4d13643e89e60e0dd951a4b636fec
Chris Oliver <excid3@gmail.com>
2017-01-20 11:53:54 -0600

    Don't blow up when OG tags aren't perfect. Fixes #8

---
+++ config/initializers/open_graph.rb
@@ -0,0 +1,5 @@
+OpenGraphReader.config.synthesize_title = true
+OpenGraphReader.config.synthesize_url = true
+OpenGraphReader.config.synthesize_image_url = true
+OpenGraphReader.config.validate_required = false
+
