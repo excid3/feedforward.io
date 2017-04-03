9e7190f1f3d8e9523524ab45d7ebdfdbebce76fb
Chris Oliver <excid3@gmail.com>
2017-04-03 12:21:30 -0500

    Remove open graph initializer

--- config/initializers/open_graph.rb
+++
@@ -1,5 +0,0 @@
-OpenGraphReader.config.synthesize_title = true
-OpenGraphReader.config.synthesize_url = true
-OpenGraphReader.config.synthesize_image_url = true
-OpenGraphReader.config.validate_required = false
-
